0dd19370ccf04146312e108cf863421f67b0efb3
Nikhil Math <erulemath@gmail.com>
2020-12-09 15:55:15 -0700

    push

---
+++ tutorial/app.js
@@ -0,0 +1,12 @@
+// console.log('hello world');
+// console.log('hello people or my friend');
+// console.log('hello people');
+// console.log('hello everyone');
+
+let name = 'Nikhil';
+let address, zip, state;
+address = "101 Main Street"
+zip = 41354
+state = "UT"
+name = "bob"
+console.log(address, zip, state, name);
