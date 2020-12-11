e3b407c9072e0e58cac4d9147a28be2e55529e99
Nikhil Math <erulemath@gmail.com>
2020-12-10 17:29:27 -0700

    25 done

--- tutorial/app.js
+++ tutorial/app.js
@@ -1,12 +1,48 @@
+const name= "john"
+const lastName = 'jordan'
+
+
+
+// let number = 40;
+// number += 5;
+// number += 5;
+// number += 5;
+// number += 5;
+// number += 5;
+
+// console.log(number);
+
+// const slices = 10;
+// const children = 3;
+// const amount = slices % children;
+
+// console.log(amount);
+
+
+// const value = 
+
 // console.log('hello world');
 // console.log('hello people or my friend');
 // console.log('hello people');
 // console.log('hello everyone');
 
-let name = 'Nikhil';
-let address, zip, state;
-address = "101 Main Street"
-zip = 41354
-state = "UT"
-name = "bob"
-console.log(address, zip, state, name);
+// let name = 'Nikhil';
+// let address, zip, state;
+// address = "101 Main Street"
+// zip = 41354
+// state = "UT"
+// name = "bob"
+// console.log(address, zip, state, name);
+
+// const number = 3
+// const number2 = 5
+
+// const add = number + number2
+// const sub = number - number2
+// const div = number / number2
+// const mul = number * number2
+
+// console.log(add)
+// console.log(sub)
+// console.log(mul)
+// console.log(div)
